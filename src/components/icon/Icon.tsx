faf12e249e3dde5268c26638ffad714af994edb0
sfgardev <sf.garol@gmail.com>
2024-01-30 19:01:48 +0700

    implemented Icon component

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,22 @@
+import sprite from "../../assets/images/icons-sprite.svg";
+
+type IconProps = {
+  iconId: string;
+  width?: string;
+  height?: string;
+  viewBox?: string;
+};
+
+export const Icon = (props: IconProps) => {
+  return (
+    <svg
+      width={props.width || "50px"}
+      height={props.height || "50px"}
+      viewBox={props.viewBox || "0 0 50 50"}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <use href={`${sprite}#${props.iconId}`} />
+    </svg>
+  );
+};
