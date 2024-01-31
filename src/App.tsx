7ea3b0b955f55ec5ea4e1ed985b600e1b8b9633c
sfgardev <sf.garol@gmail.com>
2024-01-31 22:07:38 +0700

    added Slogan component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Skills } from "./layout/sections/skills/Skills";
 import { Works } from "./layout/sections/works/Works";
 import { Testimony } from "./layout/sections/testimony/Testimony";
 import { Contact } from "./layout/sections/contact/Contact";
+import { Slogan } from "./layout/sections/slogan/Slogan";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Works />
       <Testimony />
       <Contact />
+      <Slogan />
     </div>
   );
 }
---
+++ src/layout/sections/slogan/Slogan.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { Button } from "../../../components/Button";
+
+export const Slogan = () => {
+  return (
+    <StyledSlogan>
+      <SectionTitle>I Am Available For Freelance</SectionTitle>
+      <Button>Hire me</Button>
+    </StyledSlogan>
+  );
+};
+
+const StyledSlogan = styled.section`
+  min-height: 30vh;
+  background-color: #51e7ba;
+`;
