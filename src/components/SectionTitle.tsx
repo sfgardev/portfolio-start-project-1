11fe16e32082fc6669c35d7438d92719f9ae3e9e
sfgardev <sf.garol@gmail.com>
2024-01-31 17:26:52 +0700

    SectionTitle component

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SectionTitle = styled.h2`
+  text-align: center;
+  font-size: 2.25rem;
+  font-weight: 600;
+  letter-spacing: 0.3125rem;
+`;
