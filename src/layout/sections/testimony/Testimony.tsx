aa3638038a67d2c291142d916dd1082a7422e94a
sfgardev <sf.garol@gmail.com>
2024-02-05 19:23:29 +0700

    added iconWrapper to Testimony component

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -3,13 +3,16 @@ import { SectionTitle } from "../../../components/SectionTitle";
 import { Icon } from "../../../components/icon/Icon";
 import { Slider } from "../../../components/slider/Slider";
 import { FlexWrapper } from "../../../components/FlexWrapper";
+import { IconWrapper } from "../skills/skill/Skill";
 
 export const Testimony = () => {
   return (
     <StyledTestimony>
       <FlexWrapper direction="column" align="center">
         <SectionTitle>Testimony</SectionTitle>
-        <Icon iconId="quote" />
+        <IconWrapper>
+          <Icon iconId="quote" />
+        </IconWrapper>
         <Slider />
       </FlexWrapper>
     </StyledTestimony>
@@ -17,6 +20,7 @@ export const Testimony = () => {
 };
 
 const StyledTestimony = styled.section`
-  min-height: 100vh;
-  background-color: #b3a8f9;
+  ${IconWrapper} {
+    margin: 3.75rem 0 4.5rem;
+  }
 `;
