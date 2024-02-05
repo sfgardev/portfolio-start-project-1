5c48e7d1bc6d8753b5e3d7f6bcbd99ee2273c07c
sfgardev <sf.garol@gmail.com>
2024-02-05 18:58:40 +0700

    Button component + styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,35 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 export const Button = styled.button`
-    
-`
+  font-family: inherit;
+  font-size: 0.875rem;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  width: 10.625rem;
+  height: 2rem;
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: "";
+    display: inline-block;
+    width: 45%;
+    height: 0.625rem;
+    background-color: ${theme.colors.accent};
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    translate: -50%;
+    transition: all 0.35s;
+    z-index: -1;
+  }
+
+  &:hover {
+    &::before {
+      width: 100%;
+      height: 100%;
+
+    }
+  }
+`;
