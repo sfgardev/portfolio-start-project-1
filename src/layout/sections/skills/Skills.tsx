fef0c1ee823b2fd052156bb0be798b9a3452d806
sfgardev <sf.garol@gmail.com>
2024-02-05 16:43:33 +0700

    added container + removed redundant styles in Skills component

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -3,48 +3,48 @@ import { FlexWrapper } from "../../../components/FlexWrapper";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Skill } from "./skill/Skill";
 import { Icon } from "../../../components/icon/Icon";
+import { Container } from "../../../components/Container";
 
 export const Skills = () => {
   return (
     <StyledSkills>
-      <SectionTitle>My Skills</SectionTitle>
-      <FlexWrapper justify="space-between" $wrap>
-        <Skill
-          title="html"
-          icon={<Icon iconId="code" />}
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-        />
-        <Skill
-          title="css3"
-          icon={<Icon iconId="css" />}
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-        />
-        <Skill
-          title="React"
-          icon={<Icon iconId="react" />}
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-        />
-        <Skill
-          title="typescript"
-          icon={<Icon iconId="typescript" />}
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-        />
-        <Skill
-          title="styled components"
-          icon={<Icon iconId="styled" />}
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-        />
-        <Skill
-          title="WEB DESIgN"
-          icon={<Icon iconId="figma" />}
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-        />
-      </FlexWrapper>
+      <Container>
+        <SectionTitle>My Skills</SectionTitle>
+        <FlexWrapper justify="space-between" $wrap>
+          <Skill
+            title="html"
+            icon={<Icon iconId="code" />}
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+          />
+          <Skill
+            title="css3"
+            icon={<Icon iconId="css" />}
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+          />
+          <Skill
+            title="React"
+            icon={<Icon iconId="react" />}
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+          />
+          <Skill
+            title="typescript"
+            icon={<Icon iconId="typescript" />}
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+          />
+          <Skill
+            title="styled components"
+            icon={<Icon iconId="styled" />}
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+          />
+          <Skill
+            title="WEB DESIgN"
+            icon={<Icon iconId="figma" />}
+            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+          />
+        </FlexWrapper>
+      </Container>
     </StyledSkills>
   );
 };
 
-const StyledSkills = styled.section`
-  background-color: #d6b1b1;
-  min-height: 100vh;
-`;
+const StyledSkills = styled.section``;
