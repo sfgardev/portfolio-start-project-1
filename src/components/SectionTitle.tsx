89f4585a705453dd04e178e72af60a34f9632bdb
sfgardev <sf.garol@gmail.com>
2024-02-05 16:41:07 +0700

    styles for sectionTitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,8 +1,24 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 export const SectionTitle = styled.h2`
+  font-family: "Josefin Sans", sans-serif;
   text-align: center;
   font-size: 2.25rem;
   font-weight: 600;
   letter-spacing: 0.3125rem;
+  margin-bottom: 5.625rem;
+  position: relative;
+
+  &::before {
+    content: "";
+    display: inline-block;
+    width: 55px;
+    height: 1px;
+    background-color: ${theme.colors.accent};
+    position: absolute;
+    bottom: -1.875rem;
+    left: 50%;
+    translate: -50%;
+  }
 `;
