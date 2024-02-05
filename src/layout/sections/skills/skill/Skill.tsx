58d9e39c8fc74ed9ca897b8de5535fc8bbc3ec52
sfgardev <sf.garol@gmail.com>
2024-02-05 16:42:02 +0700

    added styling for Skill component

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
+import { FlexWrapper } from "../../../../components/FlexWrapper";
 
 type SkillProps = {
   title: string;
@@ -10,19 +11,45 @@ type SkillProps = {
 export const Skill = (props: SkillProps) => {
   return (
     <StyledSkill>
-      {props.icon}
-      <SkillTitle>{props.title}</SkillTitle>
-      <SkillText>{props.text}</SkillText>
+      <FlexWrapper direction="column" align="center">
+        <IconWrapper>{props.icon}</IconWrapper>
+        <SkillTitle>{props.title}</SkillTitle>
+        <SkillText>{props.text}</SkillText>
+      </FlexWrapper>
     </StyledSkill>
   );
 };
 
 const StyledSkill = styled.div`
-  width: 30%;
-  background-color: #fff;
-  margin: 10px;
+  width: 23.75rem;
+  padding: 3.875rem 1.25rem 2.5rem;
 `;
 
-const SkillTitle = styled.h3``;
+const IconWrapper = styled.div`
+  position: relative;
 
-const SkillText = styled.p``;
+  &::before {
+    content: "";
+    width: 5rem;
+    height: 5rem;
+    background-color: rgba(255, 255, 255, 0.1);
+    position: absolute;
+    rotate: 45deg;
+    top: 50%;
+    left: 50%;
+    translate: -50% -50%;
+  }
+`;
+
+const SkillTitle = styled.h3`
+  margin: 4.375rem 0 1rem;
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 1rem;
+  letter-spacing: 0.06rem;
+  text-transform: uppercase;
+`;
+
+const SkillText = styled.p`
+  font-size: 0.875rem;
+  text-align: center;
+`;
