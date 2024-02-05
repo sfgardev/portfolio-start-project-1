deb366b92930af4cb10d5cbbc8918e77f7185699
sfgardev <sf.garol@gmail.com>
2024-02-05 18:58:10 +0700

    created Link component + styling

---
+++ src/components/Link.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { theme } from "../styles/Theme";
+
+export const Link = styled.a`
+  font-size: 0.88rem;
+  letter-spacing: 0.06rem;
+  text-transform: uppercase;
+  padding: 0.625rem;
+  position: relative;
+  z-index: 0;
+
+  &:hover {
+    &::before {
+      height: 0.63rem;
+    }
+  }
+
+  &::before {
+    content: "";
+    height: 0;
+    /* height: 0.63rem; */
+    background-color: ${theme.colors.accent};
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 5px;
+    z-index: -1;
+    transition: all 0.35s;
+  }
+`;
