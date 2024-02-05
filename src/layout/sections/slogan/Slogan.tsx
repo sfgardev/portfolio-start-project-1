f2abcb026e5ac0e6397fa7e7c479d9607afe201c
sfgardev <sf.garol@gmail.com>
2024-02-05 21:50:03 +0700

    added styling

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,17 +1,20 @@
 import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/Button";
+import { Container } from "../../../components/Container";
+import { FlexWrapper } from "../../../components/FlexWrapper";
 
 export const Slogan = () => {
   return (
     <StyledSlogan>
-      <SectionTitle>I Am Available For Freelance</SectionTitle>
-      <Button>Hire me</Button>
+      <Container>
+        <FlexWrapper direction="column" align="center">
+          <SectionTitle>I Am Available For Freelance</SectionTitle>
+          <Button>Hire me</Button>
+        </FlexWrapper>
+      </Container>
     </StyledSlogan>
   );
 };
 
-const StyledSlogan = styled.section`
-  min-height: 30vh;
-  background-color: #51e7ba;
-`;
+const StyledSlogan = styled.section``;
