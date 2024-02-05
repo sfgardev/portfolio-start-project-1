14e58c54d5827ee5ed96a67f7d30510c47f085d1
sfgardev <sf.garol@gmail.com>
2024-02-05 21:20:39 +0700

    added styling to form component

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Button } from "../Button";
+import { theme } from "../../styles/Theme";
 
 export const Form = () => {
   return (
@@ -13,11 +14,36 @@ export const Form = () => {
 };
 
 const StyledForm = styled.form`
-  max-width: 500px;
+  max-width: 33.75rem;
+  width: 100%;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  align-items: center;
+  gap: 1rem;
+
+  textarea {
+    resize: none;
+    height: 9.69rem;
+  }
 `;
 
-const Field = styled.input``;
+const Field = styled.input`
+  width: 100%;
+  background-color: ${theme.colors.secondaryBg};
+  border: 1px solid ${theme.colors.borderColor};
+  padding: 0.4375rem 0.9375rem;
+  font-family: inherit;
+  color: ${theme.colors.font};
+  font-size: 0.75rem;
+  letter-spacing: 5%;
+
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.borderColor};
+  }
+
+  &::placeholder {
+    color: ${theme.colors.placeholderColor};
+    text-transform: capitalize;
+  }
+`;
