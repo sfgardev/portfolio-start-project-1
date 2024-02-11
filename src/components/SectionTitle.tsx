5e560243d39ecd737ce2ebf7cc862ad1a76ab39a
sfgardev <sf.garol@gmail.com>
2024-02-11 17:01:47 +0700

    media for sectionTitle + responsive typography

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 import { theme } from "../styles/Theme";
+import { font } from "../styles/Common";
 
 export const SectionTitle = styled.h2`
-  font-family: "Josefin Sans", sans-serif;
+  ${font({
+    family: '"Josefin Sans", sans-serif',
+    weight: 600,
+    fMax: 2.25,
+    fMin: 1.875,
+  })}
+
   text-align: center;
-  font-size: 2.25rem;
-  font-weight: 600;
   letter-spacing: 0.3125rem;
   margin-bottom: 5.625rem;
   position: relative;
@@ -17,8 +22,12 @@ export const SectionTitle = styled.h2`
     height: 1px;
     background-color: ${theme.colors.accent};
     position: absolute;
-    bottom: -1.875rem;
+    bottom: -30px;
     left: 50%;
     translate: -50%;
+
+    @media ${theme.media.mobile} {
+      bottom: -24px;
+    }
   }
 `;
