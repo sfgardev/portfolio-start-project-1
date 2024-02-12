9f272aaa08dab270a1526350c52cc5af2eff8525
sfgardev <sf.garol@gmail.com>
2024-02-12 17:41:59 +0700

    added min width to body

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -21,6 +21,7 @@ export const GlobalStyle = createGlobalStyle`
         -moz-osx-font-smoothing: grayscale;
         color: ${theme.colors.font};
         line-height: 1.2;
+        min-width: 22.5rem;
     }
 
     code {
