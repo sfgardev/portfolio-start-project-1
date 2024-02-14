504de87c126519bb288ed24c6c875c2b5858a7f5
sfgardev <sf.garol@gmail.com>
2024-02-14 17:20:22 +0700

    mobileMenu open/close functionality

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "../menu/Menu";
 import { S } from "../HeaderMenu_Styles";
 
 export const MobileMenu: React.FC<{ menuItems: string[] }> = (props) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuIsOpen((open) => !open);
+  };
+
+  const handleCloseMobileMenu = () => {
+    setMenuIsOpen(false);
+  };
+
   return (
     <S.MobileMenu>
-      <S.BurgerButton isOpen={false}>
+      <S.BurgerButton isOpen={menuIsOpen} onClick={toggleMenu}>
         <span></span>
       </S.BurgerButton>
-      <S.MobileMenuPopup isOpen={false}>
+      <S.MobileMenuPopup isOpen={menuIsOpen} onClick={handleCloseMobileMenu}>
         <Menu menuItems={props.menuItems} />
       </S.MobileMenuPopup>
     </S.MobileMenu>
