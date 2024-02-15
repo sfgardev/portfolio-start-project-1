9fda4cd4a8ad38b5dbc31f5363e88126ca1159e5
sfgardev <sf.garol@gmail.com>
2024-02-15 12:26:03 +0700

    added tilt hover effect for photo on main section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -5,6 +5,7 @@ import { Container } from "../../../components/Container";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { S } from "./Main_Styles";
 import Typewriter from "typewriter-effect";
+import Tilt from "react-parallax-tilt";
 
 export const Main: React.FC = () => {
   return (
@@ -28,13 +29,22 @@ export const Main: React.FC = () => {
             </S.MainTitle>
           </div>
 
-          <S.PhotoWrapper>
-            <picture>
-              <source srcSet={svetaImgWebp} type="image/webp" />
-              <source srcSet={svetaImgJpeg} type="image/jpeg" />
-              <S.Photo src={svetaImgJpeg} alt="A girl with flowers" />
-            </picture>
-          </S.PhotoWrapper>
+          <Tilt
+            tiltMaxAngleX={40}
+            tiltMaxAngleY={40}
+            perspective={800}
+            transitionSpeed={1500}
+            scale={1.1}
+            gyroscope={true}
+          >
+            <S.PhotoWrapper>
+              <picture>
+                <source srcSet={svetaImgWebp} type="image/webp" />
+                <source srcSet={svetaImgJpeg} type="image/jpeg" />
+                <S.Photo src={svetaImgJpeg} alt="A girl with flowers" />
+              </picture>
+            </S.PhotoWrapper>
+          </Tilt>
         </FlexWrapper>
       </Container>
     </S.Main>
